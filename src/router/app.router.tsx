5f20e44b8801510020e6ca11ac9a07be767cf26b
jose Angel <josegallardo.cor@gmail.com>
2026-03-09 23:48:02 -0600

    ferat: auth layout y router de auth

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -3,6 +3,9 @@ import HomePage from '../bookit/pages/HomePages';
 import { BookitLayout } from "../bookit/layouts/BookitLayout";
 import ResourcePage from "../bookit/resource/ResourcePage";
 import Checkout from "../bookit/pages/Checkout";
+import AuthLayout from "../auth/layouts/AuthLayout";
+import LoginPage from "../auth/pages/LoginPage";
+import RegisterPage from "../auth/pages/RegisterPage";
 
 
 export const AppRouter = createBrowserRouter([
@@ -21,7 +24,26 @@ export const AppRouter = createBrowserRouter([
             {
                 path: "/checkout",
                 element: <Checkout/>
-            }
+            },
+        ]
+    },
+    // Auth routes
+    {
+        path:'/auth',
+        element: <AuthLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Navigate to="/auth/loing"/>
+            },
+            {
+                path: 'login',
+                element: <LoginPage/>
+            },
+            {
+                path: 'register',
+                element: <RegisterPage/>
+            },
         ]
     },
     {
